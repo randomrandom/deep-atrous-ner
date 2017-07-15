- fixed annoying embedding bug - added weights to precision and recall calculation - improvements in default parameters - added custom accuracy method - code refactoring

data/base_data_loader.py
@@ -5,6 +5,7 @@ import numpy as np
 import sugartensor as tf
 from tensorflow.contrib.tensorboard.plugins import projector
 
+from data.preprocessors.base_preprocessor import BasePreprocessor
 from data.preprocessors.conll_preprocessor import ConllPreprocessor
 
 __author__ = 'george.val.stoyan0v@gmail.com'
@@ -14,7 +15,7 @@ class BaseDataLoader(object):
     _CSV_DELIM = ","
     _DEFAULT_SKIP_HEADER_LINES = 0
     _name = "data_loader"
-    _num_threads = 2  # 32
+    _num_threads = 32  # 32
     _batch_size = 16  # 64
     _min_after_dequeue = _batch_size * _num_threads
     _capacity = _min_after_dequeue + (_num_threads + 2) * _batch_size  # as recommended in tf tutorial
@@ -23,20 +24,17 @@ class BaseDataLoader(object):
     _TABLE_CHUNK = ConllPreprocessor.VOCABULARY_PREFIX + ConllPreprocessor.VOCABULARY_CHUNK
     _TABLE_ENTITY = ConllPreprocessor.VOCABULARY_PREFIX + ConllPreprocessor.VOCABULARY_ENTITY
 
-    DEFAULT_TEST_SPLIT = .1
     DEFAULT_MAX_DATA_LENGTH = 240
     DEFAULT_VOCABULARY_SIZE = 50000
-    DEFAULT_PRETRAINED_EMBEDDINGS = 'data/embeddings/glove.6B.300d.txt'
+    DEFAULT_PRETRAINED_EMBEDDINGS = 'model/embeddings/glove.6B.300d.txt'
 
     DEFAULT_META_DATA_FILE = 'metadata.tsv'
-    DEFAULT_METADATA_DIR = 'asset/train/'
+    DEFAULT_METADATA_DIR = BasePreprocessor.DEFAULT_METADATA_DIR
 
     def __init__(self, record_defaults, field_delim, data_column, bucket_boundaries, file_names,
-                 skip_header_lines=_DEFAULT_SKIP_HEADER_LINES,
-                 num_threads=_num_threads, batch_size=_batch_size, min_after_dequeue=_min_after_dequeue,
-                 capacity=_capacity, used_for_test_data=False, meta_file=DEFAULT_META_DATA_FILE,
-                 save_dir=DEFAULT_METADATA_DIR, table=None, table_pos=None, table_chunk=None, table_entity=None,
-                 name=_name):
+                 skip_header_lines=_DEFAULT_SKIP_HEADER_LINES, num_threads=_num_threads, batch_size=_batch_size,
+                 used_for_test_data=False, meta_file=DEFAULT_META_DATA_FILE, save_dir=DEFAULT_METADATA_DIR, table=None,
+                 table_pos=None, table_chunk=None, table_entity=None, name=_name):
         self.__file_names = file_names
         self.__field_delim = field_delim
         self.__record_defaults = record_defaults
@@ -46,9 +44,10 @@ class BaseDataLoader(object):
         self.__vocabulary_file = None
 
         self._used_for_test_data = used_for_test_data
-        self._min_after_dequeue = min_after_dequeue
+        self.num_threads = num_threads
         self._batch_size = batch_size
-        self._capacity = capacity
+        self._min_after_dequeue = self._batch_size * self.num_threads
+        self._capacity = self._min_after_dequeue + (self.num_threads + 2) * self._batch_size
         self._name = name
 
         self.meta_file = meta_file
@@ -59,7 +58,6 @@ class BaseDataLoader(object):
         self.table_chunk = table_chunk
         self.table_pos = table_pos
         self.table_entity = table_entity
-        self.num_threads = num_threads
         self.vocabulary_size = 0
         self.train_size = 0
         self.test_size = 0
@@ -255,9 +253,9 @@ class BaseDataLoader(object):
         pre_trained_emb = np.random.uniform(-0.05, 0.05, (self.vocabulary_size, embed_dim))
         with open(file_name, 'r', encoding='utf-8') as emb_file:
             mapped_words = 0
-
             dictionary = ConllPreprocessor.read_vocabulary(self.__vocabulary_file, self.__field_delim)
             missing_words = dictionary.copy()
+            invalid_words = 0
 
             for line in emb_file.readlines():
                 row = line.strip().split(' ')
@@ -268,10 +266,16 @@ class BaseDataLoader(object):
                     'Embedding dimension should be same as the one in the pre-trained embeddings.'
 
                 if word in dictionary:
+                    vector = np.array([float(val) for val in row[1:]])
+                    if len(vector) != embed_dim:
+                        invalid_words += 1
+                        continue
+
                     mapped_words = mapped_words + 1
-                    pre_trained_emb[dictionary[word] - 1] = row[1:]
+                    pre_trained_emb[dictionary[word]] = vector
                     del missing_words[word]
 
+            print('Invalid words count: %d' % invalid_words)
             print('Mapped words to pre-trained embeddings: %d' % mapped_words)
 
             # TODO: should do some updates in voca_size if mapped words are less, currently missing words are random embeddings which are not going to be trained

model/model.py
@@ -12,13 +12,14 @@ GLOVE_6B_50d_EMBEDDINGS = 'glove.6B.50d.txt'
 GLOVE_6B_100d_EMBEDDINGS = 'glove.6B.100d.txt'
 GLOVE_6B_200d_EMBEDDINGS = 'glove.6B.200d.txt'
 GLOVE_6B_300d_EMBEDDINGS = 'glove.6B.300d.txt'
+GLOVE_42B_300d_EMBEDDINGS = 'glove.42B.300d.txt'
 
 embedding_dim = 300  # 300 # embedding dimension
 latent_dim = 256  # 256 # hidden layer dimension
 num_blocks = 2  # 2 # dilated blocks
 reg_type = 'l2'  # type of regularization used
 default_dout = 0.5  # define the default dropout rate
-use_pre_trained_embeddings = False  # whether to use pre-trained embedding vectors
+use_pre_trained_embeddings = True  # whether to use pre-trained embedding vectors
 pre_trained_embeddings_file = EMBEDDINGS_DIR + GLOVE_6B_300d_EMBEDDINGS  # the location of the pre-trained embeddings
 num_labels = 6
 num_pos = 6
@@ -150,20 +151,53 @@ def rnn_model(x, num_classes, is_test=False):
     return res
 
 
-def init_custom_embeddings(name, embeddings_matrix, summary=True,
-                           trainable=False):
+@tf.sg_sugar_func
+def ner_accuracy(tensor, opt):
+    r"""Returns accuracy of predictions.
+
+    Args:
+      tensor: A `Tensor`. Probability distributions or unscaled prediction scores.
+      opt:
+        target: A 'Tensor`. Labels.
+
+    Returns:
+      A `Tensor` of the same shape as `tensor`. Each value will be 1 if correct else 0. 
+
+    For example,
+
+    ```
+    tensor = [[20.1, 18, -4.2], [0.04, 21.1, 31.3]]
+    target = [[0, 1]]
+    tensor.sg_accuracy(target=target) => [[ 1.  0.]]
+    ```
+    """
+    assert opt.target is not None, 'target is mandatory.'
+    opt += tf.sg_opt(k=1)
+
+    # # calc accuracy
+    out = tf.identity(tf.equal(tensor.sg_argmax(), tf.cast(opt.target, tf.int64)).sg_float(), name='acc')
+    # out = tf.identity(tf.nn.in_top_k(tensor, opt.target, opt.k).sg_float(), name='acc')
+
+    # masking padding
+    if opt.mask:
+        out += tf.equal(opt.target, tf.zeros_like(opt.target)).sg_float()
+
+    return out
+
+
+tf.sg_inject_func(ner_accuracy)
+
+
+def init_custom_embeddings(name, embeddings_matrix, summary=True, trainable=False):
     """
     Initializes the embedding vector with custom preloaded embeddings
     """
 
     embedding = np.array(embeddings_matrix)
-    w = tf.get_variable(name, shape=embedding.shape,
-                        initializer=tf.constant_initializer(embedding),
-                        trainable=trainable)
+    emb = tf.get_variable(name, shape=embedding.shape, initializer=tf.constant_initializer(embedding),
+                          trainable=trainable)
 
     if summary:
-        tf.sg_summary_param(w)
-
-    emb = w
+        tf.sg_summary_param(emb)
 
     return emb

test.py
@@ -46,7 +46,7 @@ with tf.sg_context(name='model'):
     # calculating precision, recall and f-1 score (more relevant than accuracy)
     predictions = classifier.sg_argmax(axis=2)
     words = data.reverse_table.lookup(test.source_words)
-    entities = data.reverse_table_entity.lookup(predictions)
+    entities = data.reverse_table_entity.lookup(test.entities)
     one_hot_predictions = tf.one_hot(predictions, num_labels, dtype=tf.float64)
     one_hot_labels = tf.one_hot(test.entities, num_labels, dtype=tf.int64)
 

train.py
@@ -1,10 +1,11 @@
 from data.conll_loader import ConllLoader
+from data.preprocessors.base_preprocessor import BasePreprocessor
 from model.model import *
 from model.trainer import classifier_train
 
 __author__ = 'georgi.val.stoyan0v@gmail.com'
 
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 
 BUCKETS = [20, 40, 80, 120, 180]
 DATA_FILE = ['./data/datasets/conll_2003/eng.train']
@@ -14,13 +15,16 @@ data = ConllLoader(BUCKETS, DATA_FILE, batch_size=BATCH_SIZE)
 validation = ConllLoader(BUCKETS, TEST_FILES, batch_size=BATCH_SIZE, table=data.table, table_pos=data.table_pos,
                          table_chunk=data.table_chunk, table_entity=data.table_entity)
 
+'''
 words, pos = tf.split(data.source_words, tf.sg_gpus()), tf.split(data.source_pos, tf.sg_gpus())
 chunks, capitals = tf.split(data.source_chunk, tf.sg_gpus()), tf.split(data.source_capitals, tf.sg_gpus())
-entities = tf.split(data.entities, tf.sg_gpus())
 
 val_words, val_pos = tf.split(validation.source_words, tf.sg_gpus()), tf.split(validation.source_pos, tf.sg_gpus())
 val_chunks, val_capitals = tf.split(validation.source_chunk, tf.sg_gpus()), tf.split(validation.source_capitals,
                                                                                      tf.sg_gpus())
+'''
+entities = tf.split(data.entities, tf.sg_gpus())
+
 val_entities = tf.split(validation.entities, tf.sg_gpus())
 
 # session with multiple GPU support
@@ -35,28 +39,34 @@ word_embedding_name = 'word_emb'
 
 if use_pre_trained_embeddings:
     embedding_matrix = data.preload_embeddings(embedding_dim, pre_trained_embeddings_file)
-    word_emb = init_custom_embeddings(name=word_embedding_name, embeddings_matrix=embedding_matrix, trainable=True)
+    word_emb = init_custom_embeddings(name=word_embedding_name, embeddings_matrix=embedding_matrix, trainable=False)
 else:
     word_emb = tf.sg_emb(name=word_embedding_name, voca_size=data.vocabulary_size, dim=embedding_dim)
 
+z_w = data.source_words.sg_lookup(emb=word_emb)
+z_p = tf.one_hot(data.source_pos, depth=num_pos)
+z_c = tf.one_hot(data.source_chunk, depth=num_chunk)
+z_cap = data.source_capitals.sg_cast(dtype=tf.float32)
 
-# data.visualize_embeddings(sess, word_emb, word_embedding_name)
+# we concatenated all inputs into one single input vector
+z_i = tf.split(tf.concat([z_w, z_p, z_c, z_cap], 2), tf.sg_gpus())
+
+v_w = validation.source_words.sg_lookup(emb=word_emb)
+v_p = tf.one_hot(validation.source_pos, depth=num_pos)
+v_c = tf.one_hot(validation.source_chunk, depth=num_chunk)
+v_cap = validation.source_capitals.sg_cast(dtype=tf.float32)
+
+# we concatenated all inputs into one single input vector
+v_i = tf.split(tf.concat([v_w, v_p, v_c, v_cap], 2), tf.sg_gpus())
+
 
 # setup the model for training and validation. Enable multi-GPU support
 @tf.sg_parallel
 def get_train_loss(opt):
     with tf.sg_context(name='model'):
-        z_w = opt.words[opt.gpu_index].sg_lookup(emb=word_emb)
-        z_p = tf.one_hot(opt.pos[opt.gpu_index], depth=num_pos)
-        z_c = tf.one_hot(opt.chunks[opt.gpu_index], depth=num_chunk)
-        z_cap = opt.capitals[opt.gpu_index].sg_cast(dtype=tf.float32)
-
-        # we concatenated all inputs into one single input vector
-        z_i = tf.concat([z_w, z_p, z_c, z_cap], 2)
-
         labels = opt.entities[opt.gpu_index]
 
-        train_classifier = rnn_model(z_i, num_labels)
+        train_classifier = rnn_model(opt.z_i[opt.gpu_index], num_labels)
         # train_classifier = decode(z_i, num_labels)
 
         # cross entropy loss with logit
@@ -71,17 +81,9 @@ def get_val_metrics(opt):
     with tf.sg_context(name='model', reuse=True):
         tf.get_variable_scope().reuse_variables()
 
-        v_w = opt.words[opt.gpu_index].sg_lookup(emb=word_emb)
-        v_p = tf.one_hot(opt.pos[opt.gpu_index], depth=num_pos)
-        v_c = tf.one_hot(opt.chunks[opt.gpu_index], depth=num_chunk)
-        v_cap = opt.capitals[opt.gpu_index].sg_cast(dtype=tf.float32)
-
-        # we concatenated all inputs into one single input vector
-        v_i = tf.concat([v_w, v_p, v_c, v_cap], 2)
-
         labels = opt.entities[opt.gpu_index]
 
-        test_classifier = rnn_model(v_i, num_labels, is_test=True)
+        test_classifier = rnn_model(opt.v_i[opt.gpu_index], num_labels, is_test=True)
         # test_classifier = decode(v_i, num_labels, test=True)
 
         # accuracy evaluation (validation set)
@@ -92,11 +94,14 @@ def get_val_metrics(opt):
         one_hot_predictions = tf.one_hot(predictions, num_labels, dtype=tf.float64)
         one_hot_labels = tf.one_hot(labels, num_labels, dtype=tf.int64)
 
+        weights = tf.not_equal(labels, tf.zeros_like(labels)).sg_float()
         precision, precision_op = tf.contrib.metrics.streaming_sparse_average_precision_at_k(one_hot_predictions,
                                                                                              one_hot_labels, 1,
+                                                                                             weights=weights,
                                                                                              name='val_precision')
+
         recall, recall_op = tf.contrib.metrics.streaming_sparse_recall_at_k(one_hot_predictions, one_hot_labels, 1,
-                                                                            name='val_recall')
+                                                                            weights=weights, name='val_recall')
 
         f1_score = (2 * (precision_op * recall_op)) / (precision_op + recall_op)
 
@@ -107,9 +112,11 @@ def get_val_metrics(opt):
         return acc, val_loss, precision_op, recall_op, f1_score
 
 
+tf.sg_init(sess)
+data.visualize_embeddings(sess, word_emb, word_embedding_name)
+
 # train
-classifier_train(sess=sess, log_interval=30, lr=1e-3, clip_grad_norm=2, save_interval=150,
-                 loss=get_train_loss(words=words, pos=pos, chunks=chunks, capitals=capitals, entities=entities)[0],
-                 eval_metric=get_val_metrics(words=val_words, pos=val_pos, chunks=val_chunks, capitals=val_capitals,
-                                             entities=val_entities)[0],
+classifier_train(sess=sess, log_interval=30, lr=3e-2, clip_grad_norm=10, optim='Adam', save_interval=150,
+                 loss=get_train_loss(z_i=z_i, entities=entities)[0],
+                 eval_metric=get_val_metrics(v_i=v_i, entities=val_entities)[0],
                  ep_size=data.num_batches, max_ep=150, early_stop=False)